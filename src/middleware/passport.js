46ad5c5c808f90f7d90e33df070c9168e7f3d4ab
Loriage <bruno.durand@epitech.eu>
2020-03-16 19:16:14 +0100

    ✨ Login with Dropbox

--- src/middleware/passport.js
+++ src/middleware/passport.js
@@ -2,21 +2,26 @@ const jwt = require('jsonwebtoken')
 const User = require('../db/models/User')
 var passport = require('passport');
 var DropboxStrategy = require('passport-dropbox-auth').Strategy;
-require("dotenv").config({ path: '../'});
+require("dotenv").config({
+    path: '../'
+});
 
 passport.use(new DropboxStrategy({
-        apiVersion: '2',
-        clientID: `${process.env.DROPBOX_CLIENT_ID}`,
-        clientSecret: `${process.env.DROPBOX_CLIENT_SECRET}`,
-        callbackURL: "https://brume-tool.herokuapp.com/oauth/dropbox/callback"
+    apiVersion: '2',
+    clientID: `${process.env.DROPBOX_CLIENT_ID}`,
+    clientSecret: `${process.env.DROPBOX_CLIENT_SECRET}`,
+    callbackURL: "https://brume-tool.herokuapp.com/oauth/dropbox/callback"
     },
-    function (accessToken, refreshToken, profile, done) {
-        // User.findOrCreate({
-        //     providerId: profile.id
-        // }, function (err, user) {
-        //     return done(err, user);
-        // });
-    }
-));
+    async (accessToken, refreshToken, profile, done) => {
+        const email = profile.emails[0].value;
+
+        const currentUser = await User.findOne({ email: email });
+            if (currentUser) {
+                return done(null, currentUser);
+            } else {
+                const newUser = await new User({ email: email, dropboxId: profile.account_id, username: profile.name.display_name}).save();
+                return done(null, newUser);
+            }
+        }));
 
 module.exports = passport
